b5d55af9d0d08d8e5405198c454fb58faed1684e
DESKTOP-BH90N68\bctone <catchkin@gmail.com>
2024-11-04 21:06:33 +0900

    pydantic.errors.PydanticImportError: BaseSettings has been moved to the pydantic-settings package.
    에러 수정

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,5 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
+from functools import lru_cache
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "VoucherGPT"
@@ -9,4 +10,8 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
 
-settings = Settings()
+@lru_cache()
+def get_settings():
+    return Settings()
+
+settings = get_settings()
