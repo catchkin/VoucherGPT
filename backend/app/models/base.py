5de0b09befc10e76e5c7dfdc18d3ae12590c3aca
DESKTOP-BH90N68\bctone <catchkin@gmail.com>
2024-11-08 10:26:34 +0900

    models/base.py 변경

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,10 +1,19 @@
-from sqlalchemy import Column, Integer, DateTime
-from sqlalchemy.sql import func
-from app.core.database import Base
+from datetime import datetime
+from typing import Any
+from sqlalchemy import Column, DateTime
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
-class BaseModel(Base):
-    __abstract__ = True
 
-    id = Column(Integer, primary_key=True, index=True)
-    created_at = Column(DateTime, server_default=func.now(), nullable=False)
-    updated_at = Column(DateTime, onupdate=func.now())
+@as_declarative()
+class Base:
+    id: Any
+    __name__: str
+
+    # Generate tablename automatically
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    # Common columns for all models
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
